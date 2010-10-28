aa891c0ae8b904793f8920ab3996d1992d5f1352
Guy Boertje <guyboertje@gmail.com>
2010-10-28 01:17:42 +0100

    added to sample app

--- app/app.rb
+++ app/app.rb
@@ -6,11 +6,17 @@ app = Lapaz::Router.application('test_app') do
 
   route(:route_name=>"purchases") do
     from Processor::Purchases, {:seq_id=>0,:name=>'start'}
-      to Processor::Contacts,{:seq_id=>1,:mux_id=>'2.1'}
-      to Processor::StockItems,{:seq_id=>1,:mux_id=>'2.2'}
-      to Processor::TemplateRenderer,{:seq_id=>2}
+      to Consumer::Forwarder,{:seq_id=>1,:forward_to=>'prices/start',:reply_to=>'purchases/render/3.3'}
+      to Processor::Contacts,{:seq_id=>1,:mux_id=>'3.1'}
+      to Processor::StockItems,{:seq_id=>1,:mux_id=>'3.2'}
+      to Processor::TemplateRenderer,{:seq_id=>2,:name=>'render'}
       to Processor::LayoutRenderer,{:seq_id=>3}
       to Consumer::MongrelConsumer,{:seq_id=>4}
+  end
+
+  route(:route_name=>"prices") do
+    from Processor::Prices, {:seq_id=>0,:name=>'start'}
+    to Consumer::ReplyToForwarder,{:seq_id=>1}
   end
 
   route(:route_name=>"mongrel_test") do
@@ -23,21 +29,32 @@ app = Lapaz::Router.application('test_app') do
       to Consumer::MongrelConsumer,{:seq_id=>1}
   end
 
+
   url_handlers do
     unrecognized { {:lapaz_route => 'errors/mongrel', :view_template=>'url_unrecognized.erb', :view_layout=>nil} }
-    build :url_pattern =>'/handlertest/purchases/:id', :lapaz_route => 'purchases/start', :view_template=>'purchases.erb', :view_layout=>'default.erb'
+    build :url_pattern =>'/handlertest/purchases/:id.:format', :lapaz_route => 'purchases/start', :view_template=>'purchases.erb', :view_layout=>'default.erb'
   end
 
 end
+
+p app.services.inspect
 
 app.run()
 
 
 =begin
+
+  route(:route_name=>"reply_to_test") do
+    from Producer::FileProducer, {:seq_id=>0,:name=>'start',:filename=>"/home/gb/dev/lapaz/lib/samples/test.data",:loop_once=>true}
+      #to Processor::Prices, {:seq_id=>1}
+      to Consumer::Forwarder,{:seq_id=>1,:forward_to=>'prices/start',:reply_to=>'reply_to_test/end'}
+      to Consumer::Stdout,{:seq_id=>2,:name=>'end'}
+  end
+
     o3 = {:route_name=>"forward_test",:seq_id=>0}
     add_route from(TestProducer,o3).
               to(Delayer,{:seq_id=>1, :delay_for=>3}).
-              to(Forwarder,{:seq_id=>2, :forward_to=>'parallel_test', :forward_at=>'purchase'})
+              to(Forwarder,{:seq_id=>2, :forward_to=>'parallel_test/purchase'})
 
     o2 = {:route_name=>"yaml_test",:seq_id=>0}
     o2[:filename] = "/home/gb/dev/lapaz/lib/samples/test.yml"
